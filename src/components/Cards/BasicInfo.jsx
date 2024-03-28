bba30e13d54ca2da0332864febac2dce57da0df6
paulpriyanshu <priyanshu.paul003@gmail.com>
2024-03-28 16:12:54 +0530

    changes

--- src/components/Cards/BasicInfo.jsx
+++ src/components/Cards/BasicInfo.jsx
@@ -8,8 +8,8 @@ function BasicInfo({props}) {
       
       <div className="flex flex-col justify-center items-center relative h-96 z-50  ">  
       <div style={{overflowWrap: 'break-word', wordWrap: 'break-word'}} className="w-9/12 h-1/3 todo-container bg-slate-200 rounded-3xl mb-2 overflow-hidden  shadow-2xl cursor-auto">
-      <h1 className='px-2 py-3 text-3xl text-black-400 font-semibold'>{props.title+props.description}</h1>
-      <h1 className='px-1 py-1 text-xl text-black-400 font-semibold' >{props.temperature}</h1>
+      <h1 className='px-2 py-2 text-3xl text-black-400 font-semibold'>{props.title+props.description}</h1>
+      <h1 className='px-2 py-2 text-l text-black-400 font-semibold' >{props.temperature}</h1>
       </div>
         
       <div style={{overflowWrap: 'break-word', wordWrap: 'break-word'}} className="w-9/12 h-5/4 todo-container bg-slate-200  top-20 rounded-3xl  overflow-hidden  shadow-2xl cursor-auto">
