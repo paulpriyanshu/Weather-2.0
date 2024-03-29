1f852da591619c0438e5f6e34bc758767667405b
paulpriyanshu <priyanshu.paul003@gmail.com>
2024-03-29 22:29:29 +0530

    some changes

--- src/components/Cards/Suggestions.jsx
+++ src/components/Cards/Suggestions.jsx
@@ -22,9 +22,9 @@ function Suggestions({props}) {
     <>
     <div style={{overflowWrap: 'break-word', wordWrap: 'break-word'}} className="w-1/2 h-1/2 absolute z-50   todo-container bg-slate-300 rounded-3xl  overflow-hidden  shadow-2xl cursor-auto">
        <h1 className='px-5 py-2 text-6xl m-5 text-black-400 font-semibold '>{props.day}</h1>
-      <h1 className='px-5 py-2 text-2xl m-5 text-black-400 font-semibold' >{props.condition}</h1>
-      <h1 className='px-5 py-2 text-2xl m-5 text-black-400 font-semibold' >{props.aqi2}</h1>
-      <h1 className='px-5 py-2 text-xl m-5 text-black-400 font-semibold' >{result}</h1>
+      <h1 className='px-5 py-1 text-2xl m-5 text-black-400 font-semibold' >{props.condition}</h1>
+      <h1 className='px-5 py-1 text-2xl m-5 text-black-400 font-semibold' >{props.aqi2}</h1>
+      <h1 className='px-5 py-1 text-xl m-5 textblack-400 font-semibold' >{result}</h1>
     </div>
     
     </>
